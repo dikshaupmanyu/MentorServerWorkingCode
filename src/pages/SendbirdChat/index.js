8078c5a127dda810bfe2f5fff3d2af9a5daeb995
diksha <diksha.novasoftcorps@gmail.com>
2020-12-16 21:25:20 -0800

    chnages

--- src/pages/SendbirdChat/index.js
+++ src/pages/SendbirdChat/index.js
@@ -5,11 +5,13 @@ import { SendBirdAction } from './SendBirdAction';
 import { SendBirdConnection } from './SendBirdConnection';
 import { timestampToTime } from './utils';
 import ImageUploader from 'react-images-upload';
+import { confirmAlert } from 'react-confirm-alert'; 
+import 'react-confirm-alert/src/react-confirm-alert.css';
+
 
 const screenHeight = window.innerHeight;
 
 const sb = new SendBirdAction();
-// const chat = new Chat();
 
 class SendbirdChat extends Component {
     constructor(props) {
@@ -23,10 +25,13 @@ class SendbirdChat extends Component {
         storageData: {},
         message: '',
         channel: '',
-        scrollTop: 0
+        scrollTop: 0,
+        selectedMessageId: '',
+        scrollToBottomHeight: ''
       }
       this.onImageDrop = this.onImageDrop.bind(this);
       this.myRef = React.createRef();
+      this.onDateEnter = this.onDateEnter.bind(this);
     }
 
     onScroll = () => {
@@ -42,8 +47,10 @@ class SendbirdChat extends Component {
       if (sData !== null) {
         this.setState({
             storageData: JSON.parse(sData),
+            scrollToBottomHeight: this.myRef.current.scrollHeight
         }, () => {
           this._connectSendbird();
+          this.scrollToBottom();
         })
       }
     }
@@ -53,7 +60,7 @@ class SendbirdChat extends Component {
     }
 
     scrollToBottom = () => {
-      this.messagesEnd.scrollIntoView({block: "end"})
+      this.myRef.current.scrollTop = this.state.scrollToBottomHeight;
     }
 
     _connectSendbird = () => {
@@ -67,15 +74,15 @@ class SendbirdChat extends Component {
         this.searchKeyword = urlKeyword;
         isInit = true;
       }
-  
+ 
       SendBirdAction.getInstance()
         .getOpenChannelList(isInit, urlKeyword)
         .then(openChannelList => {
           this.setState({
-            channel: openChannelList[0]
+            channel: openChannelList[2]
           }, () => {
             SendBirdAction.getInstance()
-            .getMessageList(openChannelList[0])
+            .getMessageList(openChannelList[2])
             .then(msgList => {
               console.log('++++++++msgList+++++++', msgList);
               this.setState({
@@ -88,103 +95,120 @@ class SendbirdChat extends Component {
 
     handleMessageChange = event => {
       this.setState({
-        message: event.target.value
+        message: event.target.value,
+        scrollToBottomHeight: this.myRef.current.scrollHeight
       })
     }
 
     sendMessage = () => {
-    //alert(this.state.message);
-    if(this.state.message != ""){
-     var formdata = new FormData();
-    formdata.append("channel", this.state.channel);
-    formdata.append("user_id", this.state.storageData.uname);
-    formdata.append("message", this.state.message);
-      var requestOptions = {
-      method: 'POST',
-      headers: {
-              'Api-Token': '71c717ba89d6ff310f997f7f731c87db2901364d'
+      if(this.state.message != ""){
+        axios({
+          method: 'POST',
+          headers: {
+            'Api-Token': '71c717ba89d6ff310f997f7f731c87db2901364d'
+          },
+          url: 'https://api-30ac907b-4526-46cc-884f-14880440ca82.sendbird.com/v3/open_channels/TradeTipsPublicTest/messages',
+          data: {
+            channel: this.state.channel,
+            user_id: this.state.storageData.uname,
+            message: this.state.message
+          }
+        }).then(response => {
+          console.log(response.data);
+          let resData = {
+            messageId: response.data.message_id,
+            _sender: {
+              nickname: response.data.user.nickname
             },
-      body: formdata,
-      redirect: 'follow'
-    };
-
-      fetch(`https://api-30ac907b-4526-46cc-884f-14880440ca82.sendbird.com/v3/open_channels/TradeTipsPublicTest/messages`, requestOptions)
-      .then(response => response.json())
-      .then(result => {
-          var datak = JSON.stringify(result);
-          //alert(datak);
-          var dataResultp = JSON.parse(datak);  
-
-          if (result){
-            console.log(result);
-            const tempdata = SendBirdAction.getInstance().sendUserMessage({
-                channel: this.state.channel,
-                message: result.message,
-                handler: (message, error) => {
-                //console.log(message);
-                this.setState({
-                  messageList: [...this.state.messageList, message],
-                  message: ''
-                  })
-                   }            
-
-              });
-          }    
-         
-        // channel: this.state.channel,
-        // message: this.state.message,
-        // handler: (message, error) => {
-        //   console.log(message);
-        //   this.setState({
-        //     messageList: [...this.state.messageList, message],
-        //     message: ''
-        //   })
-        // }
-     }); 
-    } else {
-      alert("Message not send Empty")
-    }      
-    
+            message: response.data.message,
+            createdAt: response.data.created_at,
+          }
+          this.setState({
+            messageList: [...this.state.messageList, resData],
+            message: ''
+          })
+        }, (error) => {
+          console.log(error);
+        });
+      } else {
+        alert("Message not send Empty")
+      } 
     }
 
     onImageDrop(picture) {
-     const sendFile = picture[0];
-     //alert(sendFile);
-     var formdata = new FormData();
-    formdata.append("message_type", "File");
-    formdata.append("user_id", this.state.storageData.uname);
-    formdata.append("file", sendFile);
-      var requestOptions = {
-      method: 'POST',
-      headers: {
-              'Api-Token': '71c717ba89d6ff310f997f7f731c87db2901364d'
-            },
-      body: formdata,
-      redirect: 'follow'
-    };
+      const sendFile = picture[0];
+      var formdata = new FormData();
+      formdata.append("message_type", "File");
+      formdata.append("user_id", this.state.storageData.uname);
+      formdata.append("file", sendFile);
+        var requestOptions = {
+        method: 'POST',
+        headers: {
+                'Api-Token': '71c717ba89d6ff310f997f7f731c87db2901364d'
+              },
+        body: formdata,
+        redirect: 'follow'
+      };
 
       fetch(`https://api-30ac907b-4526-46cc-884f-14880440ca82.sendbird.com/v3/open_channels/TradeTipsPublicTest/messages`, requestOptions)
       .then(response => response.json())
       .then(result => {
-          var datak = JSON.stringify(result);
-          //alert(datak);
-          var dataResultp = JSON.parse(datak); 
         if (result) {
           console.log(result);
-        const tempMessage = SendBirdAction.getInstance().sendFileMessage({
-          channel: this.state.channel,
-          file: sendFile,
-          handler: (message, error) => {
-            this.setState({
-              messageList: [...this.state.messageList, message]
-            })
+          let resData = {
+            messageId: result.message_id,
+            _sender: {
+              nickname: result.user.nickname
+            },
+            url: result.file.url,
+            createdAt: result.created_at,
           }
-        });
-
-       }
-
+          this.setState({
+            messageList: [...this.state.messageList, resData],
+            message: ''
+          })
+        }
       });
-      
+    }
+
+    onDateEnter = message => {
+      this.setState({
+        selectedMessageId: message.messageId
+      })
+    }
+
+    onDateLeave = message => {
+      this.setState({
+        selectedMessageId: ''
+      })
+    }
+
+    deleteMessage = () => {
+      confirmAlert({
+        title: 'Please Confirm',
+        message: 'Are you sure to do delete this?',
+        buttons: [
+          {
+            label: 'Yes',
+            onClick: () => {
+              axios({
+                method: 'DELETE',
+                headers: {
+                  'Api-Token': '71c717ba89d6ff310f997f7f731c87db2901364d'
+                },
+                url: `https://api-30ac907b-4526-46cc-884f-14880440ca82.sendbird.com/v3/open_channels/TradeTipsPublicTest/messages/${this.state.selectedMessageId}`,
+              }).then(response => {
+                console.log(response.data);
+              }, (error) => {
+                console.log(error);
+              });
+            }
+          },
+          {
+            label: 'No',
+          }
+        ]
+      })
     }
 
     render() {
@@ -197,16 +221,16 @@ class SendbirdChat extends Component {
                   <div className="col-md-9 mx-auto" style={{height : "650px"}}>
                     <div class="chat-main-root" style={{border: '0.5px solid #898989'}}>
                       <div class="chat-main">
-                        <div class="chat-body" 
+                        <div class="chat-body"
                           ref={this.myRef}
                           onScroll={this.onScroll}
                         >
                           {this.state.messageList.length > 0 && this.state.messageList.map((message, key) => {
                             return (
-                              <div id={message.messageId} class="chat-message" data-req-id={message.messageId}>
+                              <div id={message.messageId} class="chat-message" data-req-id={message.messageId} key={key}>
                                 <div class="message-content">
                                   <div class="message-nickname">
-                                    { message._sender.nickname} : 
+                                    { message._sender.nickname} :
                                   </div>
                                   {message.name &&
                                     <div class="message-content is-file">
@@ -221,7 +245,16 @@ class SendbirdChat extends Component {
                                   <div class="time">
                                     {timestampToTime(message.createdAt)}
                                   </div>
-                                  {message.plainUrl &&
+                                  {/* {(message.messageId !== this.state.selectedMessageId) ? 
+                                    <div class="time" onMouseEnter={() => this.onDateEnter(message)} onMouseLeave={() => this.onDateLeave(message)}>
+                                      {timestampToTime(message.createdAt)}
+                                    </div>
+                                  :
+                                    <div class="time" onClick={() => this.deleteMessage()} onMouseEnter={() => this.onDateEnter(message)} onMouseLeave={() => this.onDateLeave(message)}>
+                                      Delete
+                                    </div>
+                                  } */}
+                                  {message.url &&
                                     <div class="image-content">
                                       <img class="image-render" src={message.url} />
                                     </div>
@@ -248,10 +281,10 @@ class SendbirdChat extends Component {
                             />
                           </label>
                           <div class="input-text">
-                            <textarea 
-                              class="input-text-area" 
-                              placeholder="Write a chat..." 
-                              value={this.state.message} 
+                            <textarea
+                              class="input-text-area"
+                              placeholder="Type a message ..."
+                              value={this.state.message}
                               onChange={this.handleMessageChange} required="required"
                             />
                             <button onClick={this.sendMessage}>Send</button>
@@ -267,3 +300,4 @@ class SendbirdChat extends Component {
 }
 
 export default SendbirdChat;
+
